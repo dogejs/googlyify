c0d7787ae4c40c09676b258e6ba45c141a78c520
Doge <doge.js@gmail.com>
2013-11-10 18:16:01 -0500

    top gifs

--- controllers/top.js
+++ controllers/top.js
@@ -6,23 +6,34 @@ var Gif = mongoose.model("Gif");
 module.exports = function (req, res, next) {
   Gif.find({likes: {"$gt": 0}, created_at: {"$gt": new Date(Date.now()-24*60*60*1000)}})
   .sort({likes: -1})
-  .limit(10)
+  .limit(20)
   .find(function (err, results) {
     if (err) console.error(err);
-    results = _.map(results, function (item) {
-      item = item.toObject();
-      var timesince = (Date.now()-item.created_at.getTime())/1000/60/60;
-      item.likesPerHour = (item.likes+1) / (timesince + 2);
-      item.frames = null;
-      delete item.frames;
-      return item;
-    });
     
-    results = _.sortBy(results, function (item) {
-      return -item.likesPerHour;
-    });
+    if (results.length < 10) {
+      Gif.find()
+      .sort({likes: -1})
+      .limit(20)
+      .find(function (err, results) {
+        if (err) console.error(err);
+        res.send(results);
+      });
+    } else {
+      results = _.map(results, function (item) {
+        item = item.toObject();
+        var timesince = (Date.now()-item.created_at.getTime())/1000/60/60;
+        item.likesPerHour = (item.likes+1) / (timesince + 2);
+        item.frames = null;
+        delete item.frames;
+        return item;
+      });
     
-    console.log(results);
-    res.send(results);
+      results = _.sortBy(results, function (item) {
+        return -item.likesPerHour;
+      });
+      
+      console.log(results);
+      res.send(results);
+    }
   });
 }
